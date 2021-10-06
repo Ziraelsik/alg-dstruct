9d47464ecb25b26392e50643f13a67e71aec784e
Denis Gubrienko <91973056+Ziraelsik@users.noreply.github.com>
2021-10-06 18:11:39 +0300

    Update SourceA.c

--- 2-GubrienkoDenis-A19/SourceA.c
+++ 2-GubrienkoDenis-A19/SourceA.c
@@ -1,121 +1,117 @@
-#include <stdio.h>
-#include <malloc.h>
-#include <stdlib.h>
-#include <string.h>
-#pragma warning(disable:4996)
-
-typedef enum {
-	memory_error,
-	null_string,
-	null_list,
-}error_t;
-
-typedef struct node_t {
-	struct node_t* next;
-	struct node_t* prev;
-	char* str;
-}node_t;
-
-typedef struct {
-	struct node_t* head;
-} list_t;
-
+#include "SourceA.h"
 
 list_t* create_list() {
 	list_t* list = (list_t*)malloc(sizeof(list_t));
 	if (list == NULL) {
-		exit(memory_error);
+		return NULL;
 	}
 	list->head = NULL;
-
 	return list;
 }
 
 node_t* create_node(char* str) {
-	if (str == NULL) {
-		exit(null_string);
+	if (str == NULL) {	
+		return NULL;
 	}
-	node_t* nodes = (node_t*)malloc(sizeof(node_t));
-	if (nodes == NULL) {
-		exit(memory_error);
+	node_t* node = (node_t*)malloc(sizeof(node_t));
+	if (node == NULL) {
+		return NULL;
 	}
-	nodes->str = str;
-	nodes->next = NULL;
-	nodes->prev = NULL;
-	return nodes;
+	node->str = str;
+	node->next = NULL;
+	node->prev = NULL;
+	return node;
 }
 
-void destroy_list(list_t* list) {
-	if (list == NULL) {
-		exit(null_list);
-	}
-	node_t* ptr = list->head;
-	node_t* next_node = NULL;
-	do {
-		next_node = ptr->next;
-		free(ptr);
-		ptr = next_node;
-	} while (ptr != NULL);
-	free(ptr);
-	free(list);
-}
-
-void add_element_list(node_t* node_s, char* str) {
-
-	if (node_s == NULL) {
-		exit(null_list);
-	}
+node_t* push_element_list(list_t* list, char* str) {
+	node_t* node = list->head;
 	if (str == NULL) {
-		exit(null_string);
+		return NULL;
 	}
-	node_t* new_nodes = (node_t*)malloc(sizeof(node_t));
+	if (node == NULL) {
+		node = (node_t*)malloc(sizeof(node_t));
+		if (node == NULL) {
+			return NULL;
+		}
+		list->head = node;
+		node->str = str;
+		node->next = NULL;
+		node->prev = NULL;
+		return node;
+	}
 
-	if (new_nodes == NULL) {
-		exit(memory_error);
-	}
-	new_nodes->str = (char*)malloc(strlen(str) + 1);
-	if (new_nodes->str == NULL) {
-		exit(memory_error);
-	}
-	strcpy(new_nodes->str, str);
+	node_t* prev_node;
 
-	new_nodes->prev = node_s;
-	new_nodes->next = NULL;
-	node_s->next = new_nodes;
+	while (node != NULL) {
+		prev_node = node;
+		node = node->next;
+	}
+
+	node = (node_t*)malloc(sizeof(node_t));
+	if (node == NULL) {
+		return NULL;
+	}
+
+	prev_node->next = node;
+	node->str = str;
+	node->next = NULL;
+	node->prev = prev_node;
+
+	return node;
 }
 
 void swap_str(node_t* a, node_t* b) {
 	if (a == NULL || b == NULL) {
-		exit(null_string);
+		return;
 	}
 	char* temp = a->str;
 	a->str = b->str;
 	b->str = temp;
-
 }
 
 void bubble_sort_list(list_t* list) {
-	if (list == NULL || list->head == NULL) {
-		exit(null_list);
+	if ((list == NULL )|| (list->head == NULL)) {
+		return;
 	}
 	int flag = 1;
-	node_t* ptr = NULL;
-	node_t* ptr_next = NULL;
+	node_t* node = NULL;
+	node_t* node_next = NULL;
 
 	while (flag) {
-		ptr = list->head;
-		ptr_next = ptr->next;
+		node = list->head;
+		node_next = node->next;
 		flag = 0;
-		while (ptr_next != NULL) {
-			if (strcmp(ptr->str, ptr_next->str) > 0) {
-				swap_str(ptr, ptr_next);
+		while (node_next != NULL) {
+			if (strcmp(node->str, node_next->str) > 0) {
+				swap_str(node, node_next);
 				flag = 1;
 			}
-			ptr = ptr_next;
-			ptr_next = ptr->next;
+			node = node_next;
+			node_next = node->next;
 		}
 	}
 }
+
+int destroy_list(list_t* list) {
+	if (list == NULL) {
+		return null_list;
+	}
+	node_t* node = (node_t*)malloc(sizeof(node_t));
+	node_t* next_node;
+	if (node == NULL) {
+		return null_list;
+	}
+	node = list->head;
+	while (node != NULL) {
+		next_node = node->next;
+		free(node);
+		node = next_node;
+	}
+	free(node);
+	free(list);
+	return 0;
+}
+
 
 
 
