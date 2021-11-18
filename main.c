df3f00ed4208d7ef51b7f494d00eb8458ba739f8
Denis Gubrienko <91973056+Ziraelsik@users.noreply.github.com>
2021-11-19 02:18:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
-
 #pragma warning(disable: 4996)
+
+/*
+ * CPU: Intel(R) Core(TM) i5-9300H 2.40 GHz
+ * RAM: 7,85 GB
+ * HDD: 930 GB
+ *
+ * Vertecies amount: 30000
+ *      Stress Test results:
+ *      Memory: 2.1 GB
+ *      DFS execution time: 27.044000 seconds
+ */
 
 typedef struct {
     int number_vertex;
@@ -16,7 +25,7 @@ list_t *create_list(int amount_vertex) {
     list_t *list = malloc(sizeof(list_t) * amount_vertex);
     assert(list != NULL);
     for (int i = 0; i < amount_vertex; i++) {
-        list[i].number_vertex = 0;
+        list[i].number_vertex = i;
         list[i].list_nearby_vertex = NULL;
         list[i].count_nearby_vertex = 0;
     }
@@ -93,7 +102,40 @@ void DFS(list_t *list, int amount_vertex, int vertex, int *result_list_vertex, i
 }
 
 
+
+void stress_test(int amount_vertex) {
+    list_t *list = create_list(amount_vertex);
+    int width = 30;
+//    printf("%i\n", amount_vertex);
+    for (int i = 0; i < amount_vertex; i++) {
+//        printf("%i: ", list[i].number_vertex);
+        for (int j = 0; j < width; j++) {
+            append_vertex(list, i, j);
+            append_vertex(list, j, i);
+//            printf("%i ", list[i].list_nearby_vertex[j]);
+        }
+//        printf("\n");
+    }
+    int *result_list_vertex = NULL;
+    result_list_vertex = malloc(sizeof(int) * amount_vertex);
+    assert(result_list_vertex != NULL);
+    result_list_vertex[0] = 0;
+    int result_count = 1;
+    int start = 0;
+    unsigned long start_time = clock();
+    DFS(list, amount_vertex, start, result_list_vertex, &result_count);
+    double time = (double) (clock() - start_time) / CLOCKS_PER_SEC;
+//     printf("%lf", time);
+    free(list);
+    free(result_list_vertex);
+
+}
+
+
+
 int main(void) {
+    //stress_test(30000);
+    
     int amount_vertex = 0;
     assert(scanf("%d", &amount_vertex) > 0);
 
@@ -114,6 +156,7 @@ int main(void) {
 
     free_list(list, amount_vertex);
     free(result_list_vertex);
+    
 
     return 0;
 }
